bf5f96c5ed14996b476a62f53a4646ae5690ce94
msudheerhyd <msudheerhyd@gmail.com>
2024-02-16 16:33:31 -0800

    Added LeetCode Problems

---
+++ src/blind75/ClimbStairs4.java
@@ -0,0 +1,21 @@
+package blind75;
+
+public class ClimbStairs4 {
+
+    public static int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+
+        int ways1 = 1, ways2 = 1;
+
+        for(int i=2; i<=n; i++) {
+            int ways = ways1 + ways2;
+            ways2 = ways1;
+            ways1 = ways;
+        }
+        return  ways1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+}
